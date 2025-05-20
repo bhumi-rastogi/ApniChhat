e93b959ab296c682920d347e10e34d779b5619b3
Bhumi <bhumi.rastogi05@gmail.com>
2025-05-20 21:31:56 +0530

    connecting pages and linking in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Property from './pages/Property'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/blog' element={<Blog/>}/>
+      <Route path='/property' element={<Property/>}/>
+    </Routes>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import '../styles/Navbar.css'
+import {Link} from 'react-router-dom'
+
 
 const Navbar = () => {
   return (
     <nav>
-        <img src='src/assets/logo.png' alt='Logo'></img>
+      <img src='src/assets/logo.png' alt='Apni Chhat'></img>
+
+      <ul className='link'>
+        <li>
+          <Link to="/"> Home </Link>
+        </li>
+
+        <li>
+          <Link to="/about"> About </Link>
+        </li>
+
+        <li>
+          <Link to="/property"> Property </Link>
+        </li>
+
+        <li>
+          <Link to="/blog"> Blog </Link>
+        </li>
+      </ul>
+
+      <div className="btn">
+        <button className='login'> Login </button>
+        <button className='sign'> Signup </button>
+      </div>
+
     </nav>
   )
 }
