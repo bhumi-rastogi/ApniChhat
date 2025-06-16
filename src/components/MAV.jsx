194470ef3c59a8847ff9e8284cc443be5fdf9423
Bhumi <bhumi.rastogi05@gmail.com>
2025-06-16 15:46:25 +0530

    mission and vision

---
+++ src/components/MAV.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/MAV.css";
+
+const Mission = () => {
+  return (
+    <div className="mav">
+      <div className="mcard mis">
+        <h3>Our Mission</h3>
+        <p>
+        Find your dream home! Explore diverse listings for buyers and renters, 
+        with an integrated mortgage calculator for easy financial planning.
+        </p>
+      </div>
+      <div className="mcard vis">
+        <h3>Our Vision</h3>
+        <p>
+        To be the most trusted and preferred real estate partner, recognized for 
+        simplifying property journeys and creating lasting value for every client.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Mission;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import History from '../components/History'
+import MAV from '../components/MAV'
 
 const About = () => {
   return (
     <>
     <Hero/>
     <History/>
+    <MAV/>
     <Footer/>
     </>
   )
